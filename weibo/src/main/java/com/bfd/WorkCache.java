7a402ed7b9f871bde49823523e2a9b71e8b8c723
tcf24 <tcf245@gmail.com>
2016-10-21 00:56:08 +0800

    home

--- weibo/src/main/java/com/bfd/WorkCache.java
+++ weibo/src/main/java/com/bfd/WorkCache.java
@@ -1,10 +1,15 @@
 package com.bfd;
 
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by BFD_303 on 2016/10/20.
  */
 public class WorkCache {
     public static List<String> keywords ;
+    public static int retry_times = 3;
+    public static BlockingQueue<Tasks> tasks = new LinkedBlockingQueue<>();
+    public static BlockingQueue<String> results = new LinkedBlockingQueue<>();
 }
