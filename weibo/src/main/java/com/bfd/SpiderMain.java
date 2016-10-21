a76e07b45c763b427b63e5ebcb851478543d880a
Chaofan.Tan <tcf245@gmail.com>
2016-10-21 19:37:34 +0800

    company

--- weibo/src/main/java/com/bfd/SpiderMain.java
+++ weibo/src/main/java/com/bfd/SpiderMain.java
@@ -1,5 +1,8 @@
 package com.bfd;
 
+import com.bfd.process.ListProcess;
+import com.bfd.process.Processor;
+import com.bfd.utils.HttpUtils;
 import com.google.gson.Gson;
 import org.apache.commons.codec.Encoder;
 import org.apache.commons.io.FileUtils;
@@ -17,20 +20,69 @@ import java.util.Properties;
  * Created by BFD_303 on 2016/10/20.
  */
 public class SpiderMain {
-    private static final Log LOG = LogFactory.getLog(SpiderMain.class);
+//    private static final Log LOG = LogFactory.getLog(SpiderMain.class);
+    private static Gson gson = new Gson();
+
+    static {
+        try {
+            Properties pro = new Properties();
+            pro.load(FileUtils.openInputStream(new File("etc/crawl-config.properties")));
+            WorkCache.charset = (String) pro.get("charset");
+            WorkCache.cookie = (String) pro.get("cookie");
+            WorkCache.target = (String) pro.get("target");
+            WorkCache.keywords = gson.fromJson("[\"光大银行\",\"黄金邮票\",\"理财早夜市\",\"阳光银行\",\"光大+信用卡\",\"光大+借记卡\",\"光大+理财\"]", List.class);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
-        PropertyConfigurator.configureAndWatch(SpiderMain.class.getClassLoader().getResource("log4j.properties").getFile());
+//        PropertyConfigurator.configureAndWatch("etc/log4j.properties");
+        System.out.println("WorkCache.keywords  size is : " + WorkCache.keywords.size());
+        try {
+            loadTask();
 
-        Properties pro = new Properties();
-        pro.load(FileUtils.openInputStream(new File(SpiderMain.class.getClassLoader().getResource("crawl-config.properties").getFile())));
+            while (WorkCache.tasks.size() > 0) {
+                System.out.println("get task size is : " + WorkCache.tasks.size());
 
-        WorkCache.keywords = gson.fromJson((String) pro.get("keyword"),List.class);
+                Tasks t = WorkCache.tasks.take();
+                String content = HttpUtils.httpGet(t.getUrl(), WorkCache.cookie, WorkCache.charset);
+                t.setHtml(content);
+                System.out.println("get content length is : " + content.length());
 
-        URLEncoder.encode("");
+                Processor p = new ListProcess();
+                p.process(t);
+
+                Thread.sleep(1000 * 60);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
 
+    public static void loadTask() {
+        try {
+            for (String s : WorkCache.keywords) {
+                String url = "http://s.weibo.com/weibo/@@@keyword@@@&typeall=1&suball=1&timescope=custom:2016-10-17-0:2016-10-24-0&Refer=g?sudaref=s.weibo.com";
+                url = url.replace("@@@keyword@@@", URLEncoder.encode(s));
+                System.out.println(URLEncoder.encode(s));
+                System.out.println("get task url is : " + url);
+                WorkCache.tasks.put(new Tasks(url, Tasks.Type.LIST, true));
+            }
+        } catch (InterruptedException e) {
+            System.out.println("beng le ma ");
+            e.printStackTrace();
+        }catch (Exception e) {
+            System.out.println("da beng le ma ");
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- weibo/src/main/java/com/bfd/process/ListProcess.java
+++ weibo/src/main/java/com/bfd/process/ListProcess.java
@@ -2,10 +2,18 @@ package com.bfd.process;
 
 import com.bfd.Tasks;
 import com.bfd.WorkCache;
+import com.bfd.utils.HttpUtils;
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by BFD_303 on 2016/10/20.
@@ -18,44 +26,91 @@ public class ListProcess extends Processor {
     }
 
     @Override
-    void process(Tasks task) throws InterruptedException {
+    public void process(Tasks task) throws InterruptedException {
+        // TODO 解析博文搜索列表页
+        packHtml(task);
 
         String html = task.getHtml();
         Document doc = Jsoup.parse(html);
         Elements elements = doc.select("div.WB_cardwrap");
 
         for (Element e : elements) {
-            String content = e.select("div.content p.comment_txt").text();
-            String name = e.select("div.content div.feed_content a.name_txt").text();
 
-            String time = e.select("div.feed_from a.W_textb").text();
 
-            Element feed = e.select("div.feed_action ul.feed_action_info").get(0);
-            String forward = feed.select("li:contains(转发)").text().replace("转发","");
-            String collect = feed.select("li:contains(收藏)").text().replace("收藏","");
-            String comment_count = feed.select("li:contains(评论)").text().replace("评论","");
+            try{
+                String content = e.select("div.content p.comment_txt").text();
+                String name = e.select("div.content div.feed_content a.name_txt").text();
+                String usercard = e.select("div.content div.feed_content").attr("a[usercard]");
 
-            task.put("content", content);
-            task.put("name", name);
-            task.put("time", time);
-            task.put("forward", forward);
-            task.put("collect", collect);
-            task.put("comment_count", comment_count);
+                String time = e.select("div.feed_from a.W_textb").text();
 
-            String resultData = gson.toJson(task);
+                Element feed = e.select("div.feed_action ul.feed_action_info").get(0);
+                String forward = feed.select("li:contains(转发)").text().replace("转发","");
+                String collect = feed.select("li:contains(收藏)").text().replace("收藏","");
+                String comment_count = feed.select("li:contains(评论)").text().replace("评论","");
 
-            WorkCache.results.put(resultData);
+                task.put("usercard",usercard);
+                task.put("content", content);
+                task.put("name", name);
+                task.put("time", time);
+                task.put("forward", forward);
+                task.put("collect", collect);
+                task.put("comment_count", comment_count);
+
+            }catch(NullPointerException e1){
+                continue;
+            }
+
+        }
+        if (task.get("usercard") != null){
+            //TODO 扩散usercard任务，下载并解析，获取粉丝数量
+            String url = "http://weibo.com/aj/user/newcard?@@@usercard@@@&type=1";
+            url = url.replace("@@@usercard@@@",(String)task.get("usercard"));
+
+            try {
+                String content = HttpUtils.httpGet(url,WorkCache.cookie,WorkCache.charset);
+                content = content.replace(")}catch(e){};","");
+                content = content.substring(23);
+                Map<String,Object> user = gson.fromJson(content,Map.class);
+                String userHtml = (String) user.get("data");
+
+                Document d = Jsoup.parse(userHtml);
+                String fans = d.select("div.related_info div.name dd div li:contains(收藏)").text().replace("收藏","");
+                task.put("fans",Integer.valueOf(fans));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         String nextpage = elements.select("div.W_pages span.list div.layer_menu_list ul").select("a[href]").text();
+            //TODO  扩散翻页任务
         int pageSize = nextpage.split(" ").length;
         if (task.isFirst) {
-            for (int i = 0; i < pageSize; i++) {
+            for (int i = 2; i <= pageSize; i++) {
                 String url = "";
                 WorkCache.tasks.put(new Tasks(url, Tasks.Type.LIST, false));
             }
         }
 
+        //TODO  解析结果保存
+        try {
+            String resultData = gson.toJson(task.getField());
+            FileUtils.writeStringToFile(new File(WorkCache.target),resultData,true);
+            System.out.println(resultData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    public void packHtml(Tasks task){
+        String html = task.getHtml();
+        Pattern p = Pattern.compile("\\{\"pid\":\"pl_weibo_direct\"[^\\}]+\\}");
+        Matcher m = p.matcher(html);
+        if (m.find()){
+            Map<String,Object> data = gson.fromJson(m.group(0),Map.class);
+            html = (String) data.get("html");
+            task.setHtml(html);
+        }
+    }
 }
